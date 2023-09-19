c4bc12294ea07c0c3fae4ed3d50e14938126a1ec
ImRedTea <avbantikov@edu.hse.ru>
2023-09-19 10:06:41 +0500

    Changed fibonacci function to use binary power and matrix to faster computations and allow to use bigger N

--- src/algorithms/fibonacci/function.py
+++ src/algorithms/fibonacci/function.py
@@ -1,7 +1,22 @@
+import classes
+
 def fibonacci(n: int) -> int:
-    if n == 1 or n == 2:
-        return 1
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    a = classes.Matrix2x2(0, 1, 1, 1)
+    b = classes.Matrix2x1(0, 1)
+    return (b * binarypow(a, n)).a1
+
+
+# returns same type as argument a, a must be able to multiply
+def binarypow(a, n: int):
+    b = []
+    while n > 1:
+        if n % 2 == 1:
+            b.append(type(a)(a))
+        a *= a
+        n //= 2
+    for i in b:
+        a *= i
+    return a
 
 
 def main(n: int):
