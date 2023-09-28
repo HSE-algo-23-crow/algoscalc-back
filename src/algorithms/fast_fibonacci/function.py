b62bf996a4155aa942f297bf5d2b9f5171212fc0
ImRedTea <avbantikov@edu.hse.ru>
2023-09-28 10:30:07 +0500

    Исправил недочёты оформления кода

--- src/algorithms/fast_fibonacci/function.py
+++ src/algorithms/fast_fibonacci/function.py
@@ -29,8 +29,8 @@ def mulmatrix2x2(a: (int, int, int, int),
         Returns:
             multiplication (tuple[int, int, int, int]): a * b
     """
-    return a[0] * b[0] + a[1] * b[2], a[0] * b[1] + a[1] * b[3],\
-           a[2] * b[0] + a[3] * b[2], a[2] * b[1] + a[3] * b[3]
+    return a[0] * b[0] + a[1] * b[2], a[0] * b[1] + a[1] * b[3], \
+        a[2] * b[0] + a[3] * b[2], a[2] * b[1] + a[3] * b[3]
 
 
 def fibonacci(n: int) -> (int, int):
