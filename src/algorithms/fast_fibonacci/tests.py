4d6c75c93d514359c53cad9a2643f353cb553f3e
Никта Агеев <nvageev_1@edu.hse.ru>
2023-10-04 15:59:33 +0500

    Добавил тесты на Exception и переписал main.py

--- src/algorithms/fast_fibonacci/tests.py
+++ src/algorithms/fast_fibonacci/tests.py
@@ -1,21 +1,23 @@
 import unittest
 
-
 from src.algorithms.fast_fibonacci.function import *
 
 
 class TestCase(unittest.TestCase):
-    numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711]
 
     def test_fibonacci(self):
         for index, number in enumerate(self.numbers):
             self.assertEqual(main(str(index + 1)), {'result': str(number)})
 
-    def test_matrix_mul_1(self):
-        self.assertEqual((5, 8), mulmatrix2x1((2, 1), (1, 2, 3, 4)))
+    def test_raiseException1(self):
+        self.assertRaises(ValueError, main, "-10")
 
-    def test_matrix_mul_2(self):
-        self.assertEqual((5, 8, 7, 10), mulmatrix2x2((2, 1, 1, 2), (1, 2, 3, 4)))
+    def test_raiseException2(self):
+        self.assertRaises(ValueError, main, "0.1")
+
+    def test_raiseException3(self):
+        self.assertRaises(ValueError, main, "a")
 
 
 if __name__ == '__main__':
