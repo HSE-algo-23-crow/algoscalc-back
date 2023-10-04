1508d16bcdadae8a1652f33df3b6daed5778fcb0
ImRedTea <avbantikov@edu.hse.ru>
2023-10-04 21:54:16 +0500

    Улучшение кода

--- src/algorithms/fast_fibonacci/function.py
+++ src/algorithms/fast_fibonacci/function.py
@@ -57,7 +57,8 @@ def fibonacci(n: int) -> (int, int):
 
 def main(n: str):
     if not n.isnumeric() or int(n) < 1 or int(n) > 2250000:
-        raise ValueError("n должно быть целым числом от 1 до 2250000 включительно")
+        raise ValueError("n должно быть целым числом от"
+                         " 1 до 2250000 включительно")
 
     return {'result': str(fibonacci(int(n))[0])}
 
--- src/algorithms/fast_fibonacci/tests.py
+++ src/algorithms/fast_fibonacci/tests.py
@@ -1,10 +1,11 @@
 import unittest
 
-from src.algorithms.fast_fibonacci.function import *
+from src.algorithms.fast_fibonacci.function import main
 
 
 class TestCase(unittest.TestCase):
-    numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711]
+    numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233,
+               377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711]
 
     def test_fibonacci(self):
         for index, number in enumerate(self.numbers):
