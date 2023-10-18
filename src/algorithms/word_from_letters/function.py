6a419dfa5c67670fceba43c12918971c4f407714
katenadolgih <katenadolgih01@gmail.com>
2023-10-19 02:17:48 +0500

    исправлены code smells

--- src/algorithms/word_from_letters/function.py
+++ src/algorithms/word_from_letters/function.py
@@ -15,7 +15,25 @@ def generate_permutations(letters_str: str) -> list[str]:
     :return: список перестановок символов входящей строки, где каждая
     перестановка строка, содержащая указанные символы
     """
-    pass
+    if not isinstance(letters_str, str):
+        raise TypeError('Переданный параметр не является строкой')
+    if len(letters_str) > LENGTH_LIMIT:
+        raise ValueError(f'Длина введенной строки превышает {LENGTH_LIMIT} символов')
+    if ' ' in letters_str:
+        raise ValueError('Введенная строка содержит пробел')
+
+    letters_str = letters_str.lower()
+
+    def permute(data, i, length):
+        if i==length:
+            yield ''.join(data)
+        else:
+            for j in range(i,length):
+                data[i], data[j] = data[j], data[i]
+                yield from permute(data, i+1, length)
+                data[i], data[j] = data[j], data[i]
+
+    return list(permute(list(letters_str), 0, len(letters_str)))
 
 
 def main(letters: str):
@@ -28,7 +46,13 @@ def main(letters: str):
     превышает максимально возможное значение
     :return: список слов
     """
-    pass
+    morph = pymorphy3.MorphAnalyzer()
+    permutations = set(generate_permutations(letters))
+    words = []
+    for permutation in permutations:
+        if morph.word_is_known(permutation):
+            words.append(permutation)
+    return {WORDS: words}
 
 
 if __name__ == '__main__':
